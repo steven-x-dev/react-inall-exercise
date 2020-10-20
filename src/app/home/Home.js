4b9621b81f82fb48ec48227ad83700e7da1c4947
steven-x-dev <steven-dev@qq.com>
2020-10-21 03:21:16 +0800

    [steven] refactor: added Feature component

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -4,32 +4,30 @@ import calculator from '../../images/calculator.png';
 import timer from '../../images/timer.png';
 import Feature from "./feature/Feature";
 
-const Home = () => {
-  return (
-    <div className="home">
-      <div className="title-area">
-        <div className="title-wrapper">
-          <p className="title-text">在线实用工具</p>
-        </div>
-      </div>
-      <div className="icon-area">
-        <div className="feature-boxes-wrapper">
-          <Feature
-            link="/calculator"
-            src={calculator}
-            alt="calculator"
-            name="计算器"
-          />
-          <Feature
-            link="/timer"
-            src={timer}
-            alt="timer"
-            name="倒计时器"
-          />
-        </div>
+const Home = () => (
+  <div className="home">
+    <div className="title-area">
+      <div className="title-wrapper">
+        <p className="title-text">在线实用工具</p>
       </div>
     </div>
-  );
-};
+    <div className="icon-area">
+      <div className="feature-boxes-wrapper">
+        <Feature
+          link="/calculator"
+          src={calculator}
+          alt="calculator"
+          name="计算器"
+        />
+        <Feature
+          link="/timer"
+          src={timer}
+          alt="timer"
+          name="倒计时器"
+        />
+      </div>
+    </div>
+  </div>
+);
 
 export default Home;
