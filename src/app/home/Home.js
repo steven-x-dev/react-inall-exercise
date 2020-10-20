7f8ef00078098ae0f782f39c89df1ac7a86aece0
steven-x-dev <steven-dev@qq.com>
2020-10-20 22:23:10 +0800

    [steven] feat: completed home page styling

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -8,21 +8,25 @@ import {Link} from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <div>
-        <img src={hero} className="hero-image" alt="background" />
-      </div>
-      <div>
-        <div className="feature-box">
-          <img src={calculator} className="calculator-image" alt="calculator icon" />
-          <p>
-            <Link to='/calculator'>计算器</Link>
-          </p>
+      <div className="title-area">
+        <div className="title-wrapper">
+          <p className="title-text">在线实用工具</p>
         </div>
-        <div className="feature-box">
-          <img src={timer} className="timer-image" alt="timer icon" />
-          <p>
-            <Link to='/timer'>倒计时器</Link>
-          </p>
+      </div>
+      <div className="icon-area">
+        <div className="feature-boxes-wrapper">
+          <div className="feature-box">
+            <Link to='/calculator'>
+              <img src={calculator} className="icon-image" alt="calculator icon" />
+              <p>计算器</p>
+            </Link>
+          </div>
+          <div className="feature-box">
+            <Link to='/timer'>
+              <img src={timer} className="icon-image" alt="timer icon" />
+              <p>倒计时器</p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
