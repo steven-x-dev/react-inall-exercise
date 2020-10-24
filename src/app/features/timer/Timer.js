650c93fb5bda6fc9a46669b303900c7438bf48c7
steven-x-dev <steven-dev@qq.com>
2020-10-24 18:42:12 +0800

    [steven] feat: completed timer page and styling

--- src/app/features/timer/Timer.js
+++ src/app/features/timer/Timer.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import './timer.less';
 
 class Timer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 'Start'
+    };
+  }
+
   render() {
+    const { display } = this.state;
     return (
       <div className='timer'>
-        This is timer
+        <div className='section input-section'>
+          <div className='time-setter'>
+            设置时间
+            <input type='number' max={9999} min={1} />
+          </div>
+          <div className='start-button'>
+            Start
+          </div>
+        </div>
+        <div className='section'>
+          <div className='display-section'>
+            {display}
+          </div>
+        </div>
       </div>
     );
   }
